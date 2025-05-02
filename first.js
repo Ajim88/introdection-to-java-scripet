5d7aa85513c1eec1f4551b0c33c2d001064082d1
ajim khan <ajkhanbd24@gmail.com>
2025-05-02 15:37:28 +0600

    if.js

---
+++ first.js
@@ -0,0 +1,16 @@
+alert("i love you medam")
+// var name =("Chumki")
+// document.write("<h1>I love Bangladesh</h1>")
+// console.log("I love Medam")
+// document.write("<h2>Name: Ajim Khan </h2> <br>")
+// document.write("<h2>Phone: 01933676667 </h2> <br>")
+// document.write("<h2>Subject: Love </h2> <br>")
+// document.write("<h2>She: Chumki Medam </h2>")
+
+// var name = "Ajim Khan <br>"
+//         document.write(name)
+//         name = "Chumki Medam"
+//         document.write(name)
+
+// var name = "Jannat";
+// var age = 23;
---
+++ if.js
@@ -0,0 +1,25 @@
+var biriyaniPrice = 250;
+
+if(3 < 10){
+    console.log('mama give me some biriyani')
+}
+
+var weight =40;
+if(weight < 20){
+    console.log('i will carry it by myself')
+}
+else{
+    console.log("i will rent a rickshaw")
+}
+
+/* Multiple.js */ 
+
+const salary = 25000;
+const isBCS = true;
+
+if(salary > 20000 ){
+    console.log('su.......patro');
+}
+else{
+    console.log('onno patro khuji')
+}
